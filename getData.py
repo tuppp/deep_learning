38e33c3c61a41667cd2e5686977e1849159cb2a1
Marco Schiwek <schiwekm@gmx.de>
2018-07-03 17:14:39 +0200

    SQL Abfrage für die 5 Städte

--- getData.py
+++ getData.py
@@ -2,21 +2,24 @@ import FunctionLibraryExtended as library
 tmp=library.getConnectionDWD()
 print(library.getMaxTempUp(30.0,tmp[0],tmp[1]))
 #Anfrage PLZ
-result=tmp[1].execute("""SELECT station_id,postcode, station_name, count(*)
-                        FROM dwd
-                         GROUP BY station_name
-                         """)
+# result=tmp[1].execute("""SELECT station_id,postcode, station_name, count(*)
+#                         FROM dwd
+#                          GROUP BY station_name
+#                          """)
 #result=tmp[1].execute("""SELECT station_id,postcode, station_name,measure_date
 #                        FROM dwd
 #                         Where station_name='Berlin-Tegel'
 #                         ORDER BY measure_date ASC
 #                         """)
+
+result=tmp[1].execute("""SELECT station_name,measure_date,average_temp
+                       FROM dwd
+                        Where station_name='BRAUNLAGE' OR station_name='POTSDAM' OR
+                        station_name='BREMEN' OR station_name='TRIER-PETRISBERG' OR station_name='FICHTELBERG'
+                        ORDER BY measure_date ASC, station_name ASC
+                        """)
+
 for line in result:
     print(line)
 
-#result=tmp[1].execute("""SELECT station_name,measure_date,average_temp
-#                        FROM dwd
-#                         Where station_name='' OR station_name='' OR station_name='' OR station_name='' OR station_name
-#                         ORDER BY measure_date ASC, station_name ASC
-#                         """)
-#
+
