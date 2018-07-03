6a7262572be24265e536aebb6b0be4b3ff540cc3
Jeff <jflintermann95@gmail.com>
2018-07-03 17:05:53 +0200

    fold

--- hyperparams.py
+++ hyperparams.py
@@ -35,8 +35,10 @@ class Hyperparams():
             "nr_convs" : nr_convs,
             "channel_sequence" : channel_sequence,
             "nr_fully_connected_layers" : 3,
-            "learning_rate" : 0.001
-        }
+            "learning_rate" : 0.001,
+            "batch_size" : 500
+            "batches_per_fold" : 10
+            }
         return hyperparams
 
 
